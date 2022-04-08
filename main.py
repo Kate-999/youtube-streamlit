1c46ead1e56491a9c2e29b3bee5970dc3972ec8b
Kate-999 <kuroyui91@gmail.com>
2022-04-08 13:44:37 +0900

    1st commit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
 import time
 
 st.title ('Streamlit 超入門')
@@ -31,14 +28,4 @@ expander2.write('問い合わせ2の回答')
 expander3 = st.expander('問い合わせ3')
 expander3.write('問い合わせ3の回答')
 
-
-# text = st.text_input('あなたの趣味を教えてください')
-# condition = st.slider('あなたの今の調子は？', 0, 100, 50)
-
-# 'あなたの趣味：', text
-# 'コンディション：', condition
-
-# if st.checkbox('Show Image'):
-#     img = Image.open('sample.jpg')
-#     st.image(img, caption='BOOK', use_column_width=True)
 
